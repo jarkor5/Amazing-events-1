8d48f1549c906c6284bf497b2c978a99fe045848
Jacobo <jacoor626@gmail.com>
2023-02-01 19:31:35 -0500

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,19 @@
+let events = data.events
+let lista = document.getElementById("carta-container")
+//console.log(data.events[0].name)
+
+for(let event of events ){
+    lista.innerHTML += ` <div class="carta">
+    <img src="${event.image}" class="carta-image">
+    <h5 class="carta-title">${event.name}</h5>
+    <p class="carta-text">${event.description}</p>
+    <a href="#" class="carta-button">View more</a>
+
+</div>`
+    console.log(lista)
+
+}
+
+
+
+ 
