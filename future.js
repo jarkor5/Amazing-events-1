74a620616f133abe6e7ca96f633f4a065315caf2
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-02-10 08:13:52 -0500

    Add files via upload

--- future.js
+++ future.js
@@ -1,17 +1,87 @@
 let events = data.events
 let lista = document.getElementById("carta-container")
-//console.log(data.events[0].name)
+let search = document.getElementById('search')
 
-for(let event of events ){
+
+
+
+function crearcard(e){
+    let template = ''
+for(let event of e ){
     if(event.date > data.currentDate){
-        lista.innerHTML += ` <div class="carta">
+        template += ` <div class="carta">
     <img src="${event.image}" class="carta-image">
     <h5 class="carta-title">${event.name}</h5>
     <p class="carta-text">${event.description}</p>
-    <a href="#" class="carta-button">View more</a>
+    <<a href="./details.html?id=${event._id}&name=${event.name}" class="carta-button">View more</a>
+        
 
 </div>`
     }
     
-
 }
+
+lista.innerHTML = template
+}
+
+crearcard(events)
+
+ let cardFilter = document.querySelector('.card-filter')
+
+ let checkboxFilter = document.querySelector('.form-container')
+ 
+
+
+ const categories = [ ...new Set(events.map( event => event.category))]
+
+ const checkfilter = document.getElementsByClassName('.checkboxfilter')
+ 
+
+
+   let checkboxContainer = ''
+ for (let category of categories){
+
+        checkboxContainer += `<div class="form-input">
+        <input type="checkbox" value="${category}" name="${category}" id="filter" class="form-input"></input>
+        <label for="${category}">${category}</label>
+        </div>`
+
+       
+     }
+
+     checkboxFilter.innerHTML += checkboxContainer
+  
+    checkboxFilter.addEventListener('change', (e) =>{
+      let filter =   eventFilter(events)
+      crearcard(filter)
+    })
+
+
+ function eventFilter(events){
+   let x = [...document.querySelectorAll('input[type="checkbox"]:checked')]
+   let y = x.map(cate => cate.value)
+   if(y.length === 0){
+    return events
+   }
+   return events.filter(filtrado => y.includes(filtrado.category))
+
+        }
+
+
+        function filterSearch(search, e){
+            let filtroArray = e.filter(filtroBusqueda => filtroBusqueda.name.toLowerCase().includes(search))
+            return filtroArray
+        
+          }
+        
+        
+          search.addEventListener('keyup', (e) =>{
+            let buscador = search[0].value.toLowerCase()
+            let buscarFunction = filterSearch(buscador, events)
+            let filtrados = eventFilter(buscarFunction)
+            console.log(e)
+            crearcard(filtrados) 
+          }
+            
+          
+          )
--- index.js
+++ index.js
@@ -1,19 +1,106 @@
+
+// data
 let events = data.events
+
+// contenedor donde paran los elementos
 let lista = document.getElementById("carta-container")
-//console.log(data.events[0].name)
 
-for(let event of events ){
-    lista.innerHTML += ` <div class="carta">
-    <img src="${event.image}" class="carta-image">
-    <h5 class="carta-title">${event.name}</h5>
-    <p class="carta-text">${event.description}</p>
-    <a href="#" class="carta-button">View more</a>
+// input search
+let search = document.getElementById('search')
 
-</div>`
-    console.log(lista)
+//contenedor de los checkbox
+ let checkboxFilter = document.querySelector('.form-container')
 
+
+// crear card
+function crearcard(e){ 
+    if(e.length === 0){
+        lista.innerHTML = `<p class="fs-4 text-white">Card not found</p>`
+    }
+    else{
+        let template = ''
+        for(let event of e ){
+            template += ` <div class="carta">
+            <img src="${event.image}" class="carta-image">
+            <h5 class="carta-title">${event.name}</h5>
+            <p class="carta-text">${event.description}</p>
+            <a href="./details.html?id=${event._id}&name=${event.name}" class="carta-button">View more</a>
+        
+        </div>`
+        
+    }
+   
+    lista.innerHTML = template
 }
 
 
 
+
+}
+
+crearcard(events)
+
+ 
+ 
+
+
+ const categories = [ ...new Set(events.map( event => event.category))]
+
+ const checkfilter = document.getElementsByClassName('.checkboxfilter')
+ 
+
+
+   let checkboxContainer = ''
+ for (let category of categories){
+
+        checkboxContainer += `<div class="form-input">
+        <input type="checkbox" value="${category}" name="${category}" id="filter" class="form-input"></input>
+        <label for="${category}">${category}</label>
+        </div>`
+
+       
+     }
+
+     checkboxFilter.innerHTML += checkboxContainer
+  
+    checkboxFilter.addEventListener('change', (e) =>{
+      let filter =   eventFilter(events)
+      crearcard(filter)
+    })
+
+
+ function eventFilter(events){
+   let x = [...document.querySelectorAll('input[type="checkbox"]:checked')]
+   let y = x.map(cate => cate.value)
+   if(y.length === 0){
+    return events
+   } else{
+    return events.filter(filtrado => y.includes(filtrado.category))
+   }
+  
+  }
+
+
+  function filterSearch(search, e){
+    let filtroArray = e.filter(filtroBusqueda => filtroBusqueda.name.toLowerCase().includes(search))
+    return filtroArray
+
+  }
+
+
+  search.addEventListener('keyup', (e) =>{
+    let buscador = search[0].value.toLowerCase()
+    let buscarFunction = filterSearch(buscador, events)
+    let filtrados = eventFilter(buscarFunction)
+    console.log(e)
+    crearcard(filtrados) 
+  }
+    
+  
+  )
+  
+
+
+
+ console.log()
  
